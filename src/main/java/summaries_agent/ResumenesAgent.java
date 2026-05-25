2975f1511b8c1132288e72b7ce0b1a28a30177d9
ARdGG <159362291+ARdGG@users.noreply.github.com>
2026-05-25 18:28:00 +0200

    Cambios resumenes agent

--- src/main/java/summaries_agent/ResumenesAgent.java
+++ src/main/java/summaries_agent/ResumenesAgent.java
@@ -11,11 +11,6 @@ public class ResumenesAgent extends AgentBase{
 		//Valores por defecto 
 		String guiasDirectory = "src/main/resources/guias";
 		
-		Object[] args = getArguments();
-        
-		if(args != null && args.length >= 1) {
-			guiasDirectory = (String) args[0];
-		}
 		System.out.println("Usando rutas:\nGuias = " + guiasDirectory);
 		
         //Configurar agente
@@ -26,11 +21,4 @@ public class ResumenesAgent extends AgentBase{
 		BusquedaResumenBehaviour searchBehaviour = new BusquedaResumenBehaviour(guiasDirectory);
 		addBehaviour(searchBehaviour);
     }
-
-	@Override
-	protected void takeDown() {
-		System.out.println("Arrivederci :D");
-
-        deregisterAgentDF();
-	}
 }

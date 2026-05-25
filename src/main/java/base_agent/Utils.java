caffc24301ca3df050e3d9a21ae303c74c81b10a
ARdGG <159362291+ARdGG@users.noreply.github.com>
2026-05-25 18:26:46 +0200

    Update Utils.java

--- src/main/java/base_agent/Utils.java
+++ src/main/java/base_agent/Utils.java
@@ -13,50 +13,46 @@ import java.io.IOException;
 import java.io.Serializable;
 
 public class Utils {
-	
+
 	protected static DFAgentDescription[] buscarAgentes(Agent agent, String tipo) {
 		DFAgentDescription template = new DFAgentDescription();
 		ServiceDescription templateSd = new ServiceDescription();
 		templateSd.setType(tipo);
 		template.addServices(templateSd);
-		
+
 		SearchConstraints sc = new SearchConstraints();
 		sc.setMaxResults(Long.MAX_VALUE);
-		
+
 		try {
 			return DFService.search(agent, template, sc);
 		} catch (FIPAException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
-	public static void enviarMensaje(Agent agent, String tipo, Object objeto) {
+
+	public static void enviarMensaje(Agent agent, String tipo, String mensaje) {
 		DFAgentDescription[] dfd = buscarAgentes(agent, tipo);
-		
-		try {
-			if(dfd != null && dfd.length > 0) {
-				ACLMessage aclMessage = new ACLMessage(ACLMessage.REQUEST);
-				
-				for(int i = 0; i < dfd.length; i++) {
-					aclMessage.addReceiver(dfd[i].getName());
-				}
-				
-				aclMessage.setOntology("ontologia");
-				aclMessage.setLanguage(new SLCodec().getName());
-				aclMessage.setEnvelope(new Envelope());
-				aclMessage.getEnvelope().setPayloadEncoding("ISO8859_1");
-				aclMessage.setContentObject((Serializable)objeto);
-				
-				agent.send(aclMessage);
+
+		if(dfd != null && dfd.length > 0) {
+			ACLMessage aclMessage = new ACLMessage(ACLMessage.REQUEST);
+
+			for(int i = 0; i < dfd.length; i++) {
+				aclMessage.addReceiver(dfd[i].getName());
 			}
-			else {
-				System.out.println("No se encontró ningún agente del tipo: " + tipo);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
+
+			aclMessage.setOntology("ontologia");
+			aclMessage.setLanguage(new SLCodec().getName());
+			aclMessage.setEnvelope(new Envelope());
+			aclMessage.getEnvelope().setPayloadEncoding("ISO8859_1");
+			aclMessage.setContent(mensaje);
+
+			agent.send(aclMessage);
+		}
+		else {
+			System.out.println("No se encontró ningún agente del tipo: " + tipo);
 		}
 	}
-
 }
+
